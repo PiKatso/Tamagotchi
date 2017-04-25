5bfeb882c7f8f02867db65c04d6166275702fc0a
Kat Gifford and Sowmya Dinavahi <sowmya@>
2017-04-25 13:54:32 -0700

    add spec for set food level

--- lib/Tamagotchi.rb
+++ lib/Tamagotchi.rb
@@ -1,10 +1,10 @@
 class Tamagotchi
 
-  define_method(:initialize) do |name,food,snooze,activity|
+  define_method(:initialize) do |name|
     @name = name
-    @food = food
-    @snooze =snooze
-    @activity = activity
+    @food = 10
+    @snooze = 10
+    @activity = 10
   end
 
   define_method(:name) do
@@ -13,6 +13,10 @@ class Tamagotchi
 
   define_method(:food_stats) do
     @food
+  end
+
+  define_method(:set_food_level) do |stat|
+    @food = stat
   end
 
   define_method(:snooze_stats) do
@@ -29,5 +33,10 @@ class Tamagotchi
     else
       return false
     end
+  end
+
+  define_method(:time_passes) do
+    @food = @food - 1
+
   end
 end
--- spec/Tamagotchi_spec.rb
+++ spec/Tamagotchi_spec.rb
@@ -5,7 +5,7 @@ require "pry"
 describe 'Tamagotchi' do
   describe("#initialize") do
   it "sets the name and life stats of new Tamagotchi" do
-    my_pet = Tamagotchi.new("chipmunk",10,10,10)
+    my_pet = Tamagotchi.new("chipmunk")
     expect(my_pet.name()).to(eq("chipmunk"))
     expect(my_pet.food_stats()).to(eq(10))
     expect(my_pet.snooze_stats()).to(eq(10))
@@ -15,13 +15,23 @@ describe 'Tamagotchi' do
 
   describe("#alive") do
     it("is alive if food stats are above 0") do
-      my_pet = Tamagotchi.new("chipmunk",10,10,10)
+      my_pet = Tamagotchi.new("chipmunk")
       expect(my_pet.alive()).to(eq(true))
     end
     it("is dead if food stats are 0") do
-      my_pet = Tamagotchi.new("chipmunk",0,10,10)
+      my_pet = Tamagotchi.new("chipmunk")
+      my_pet.set_food_level(0)
       expect(my_pet.alive()).to(eq(false))
     end
   end
 
+  describe("#time_passes") do
+    it("decreases the amount of food the Tamagotchi has by 1") do
+      my_pet = Tamagotchi.new("chipmunk")
+      my_pet.time_passes()
+      expect(my_pet.food_stats()).to(eq(9))
+      expect(my_pet.snooze_stats()).to(eq(9))
+      expect(my_pet.activity_stats()).to(eq(9))
+    end
+  end
 end
